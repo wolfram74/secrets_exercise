2af2ef78bbe7485b07b9cc0c27448d9dae50dcc6
wolfram74 <phaugen@gmail.com>
2015-05-31 14:39:24 -0700

    experimented with config files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @farts = ENV["thing_doer"]
+    ENV.each{|k, v| p [k, v]}
+    p Figaro.env.thing_doer
+    @stuffs = "nuffs"
   end
 
 end
